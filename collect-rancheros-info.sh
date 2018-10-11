8f3e2f8373291fa9f8f6727d0ed7b95e145a3cea
kingsd041 <nicholas_ksd@hotmail.com>
2018-10-11 14:26:35 +0800

    echo

--- collect-rancheros-info.sh
+++ collect-rancheros-info.sh
@@ -56,6 +56,7 @@ fi
 
 tar -c -f /tmp/rancheros_export_$ARCHIVE -C $dest_dir  . >/dev/null 2>&1
 
+/bin/echo "*********************************************************"
 /bin/echo -e "The RancherOS config and log are successfully exported. \
 \nPlease check the /tmp/rancheros_export_$ARCHIVE."
-
+/bin/echo "*********************************************************"

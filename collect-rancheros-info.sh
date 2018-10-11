49793f0d71ab941a4357241ca8fd1fc80fbfd2a6
kingsd041 <nicholas_ksd@hotmail.com>
2018-10-11 13:51:18 +0800

    cancel sudo

--- collect-rancheros-info.sh
+++ collect-rancheros-info.sh
@@ -21,40 +21,40 @@ ARCHIVE=$DATE.tar
 for i in $dest_conf_dir $dest_log_dir;
 do
   if [ ! -d $i ]; then
-    sudo mkdir -p -m 777 $i
+    mkdir -p  $i
   fi
 done
 
 
 # Hidden ssh-rsa
 hiddenSshRsa(){
-     sudo sed -i 's/ssh-rsa.*$/ssh-rsa .../g' $1
+     sed -i 's/ssh-rsa.*$/ssh-rsa .../g' $1
 }
 
 # Export boot log
-sudo cp -arf $boot_log_src_dir $dest_log_dir
+cp -arf $boot_log_src_dir $dest_log_dir
 # Export rancheros config
-sudo ros c export -o $dest_conf_dir/ros-config-export.conf
-sudo ros -v > $dest_conf_dir/ros-version
-sudo uname -r > $dest_conf_dir/kernel-version
-sudo system-docker info > $dest_conf_dir/system-docker-info
-sudo docker info > $dest_conf_dir/docker-info
-sudo cat /proc/mounts > $dest_conf_dir/proc-mounts
-sudo cat /proc/1/mounts > $dest_conf_dir/proc-1-mounts
-sudo cat /proc/cmdline > $dest_conf_dir/cmdline
-sudo dmesg > $dest_conf_dir/dmesg.log
-#sudo ls $conf_file_src_dir | \
+ros c export -o $dest_conf_dir/ros-config-export.conf
+ros -v > $dest_conf_dir/ros-version
+uname -r > $dest_conf_dir/kernel-version
+system-docker info > $dest_conf_dir/system-docker-info
+docker info > $dest_conf_dir/docker-info
+cat /proc/mounts > $dest_conf_dir/proc-mounts
+cat /proc/1/mounts > $dest_conf_dir/proc-1-mounts
+cat /proc/cmdline > $dest_conf_dir/cmdline
+dmesg > $dest_conf_dir/dmesg.log
+#ls $conf_file_src_dir | \
 #        grep -v "pem" | \
-#        xargs -i sudo cp -r $conf_file_src_dir/{} $dest_conf_dir
-cd $conf_file_src_dir && sudo cp -rf `sudo ls  | grep -E -v "^(pem)$"` $dest_conf_dir
-sudo cp -arf $os_config_dir $dest_conf_dir
+#        xargs -i cp -r $conf_file_src_dir/{} $dest_conf_dir
+cd $conf_file_src_dir && cp -rf `ls  | grep -E -v "^(pem)$"` $dest_conf_dir
+cp -arf $os_config_dir $dest_conf_dir
 
 hiddenSshRsa $dest_conf_dir/ros-config-export.conf
 if [ -f  $dest_conf_dir/metadata ]; then
     hiddenSshRsa $dest_conf_dir/metadata
 fi
 
-sudo tar -c -f /tmp/rancheros_export_$ARCHIVE -C $dest_dir  . >/dev/null 2>&1
+tar -c -f /tmp/rancheros_export_$ARCHIVE -C $dest_dir  . >/dev/null 2>&1
 
 echo -e "\n"
 echo -e "The RancherOS config and log are successfully exported. \
